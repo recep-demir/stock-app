c27a9748f80ef337dd9386f7d7488249e3777af1
recep-demir <demir.rp@gmail.com>
2025-02-05 19:37:01 +0100

    add redux persist in store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+// import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
-export default store;
+
+export   let persistor = persistStore(store) //named export istedigimiz kadar export edilir import edilirken {} icine alinir.
+
+export default store; //default export it can just create onyly one.
