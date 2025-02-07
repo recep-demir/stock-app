d414cec7a12dda5ae9f127932479bebeee6a15b5
recep-demir <demir.rp@gmail.com>
2025-02-07 19:03:14 +0100

    add firmsCard

--- src/components/FirmsCard.jsx
+++ src/components/FirmsCard.jsx
@@ -1,15 +1,78 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const FirmsCard = () => {
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: 'rotate(0deg)',
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: 'rotate(180deg)',
+      },
+    },
+  ],
+}));
 
-    const {firms}=useSelector((state)=>state.stock)
+export default function FirmsCard({_id,name,phone,address,image }) {
+  const [expanded, setExpanded] = React.useState(false);
 
-    console.log("firms",firms)
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <div>FirmsCard</div>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title={name}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {phone}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        
+      </CardActions>
 
-export default FirmsCard
+    </Card>
+  );
+}
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { useEffect } from 'react'
 import useStockCall from '../hook/useStockCall'
 import FirmsCard from './../components/FirmsCard';
+import { Button, Container, Grid, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 
 const Firms = () => {
 const {getFirms}=useStockCall()
-  
+const {firms}=useSelector(state=>state.stock)
+
+
   useEffect(()=>{
   getFirms()
   },[])
@@ -13,11 +17,28 @@ const {getFirms}=useStockCall()
 
 
   return (
-    <div>Firms
+   <Container>
+    <Typography variant='h4' color="secondary.second" align='center'> 
+      Firms
 
-<FirmsCard/>
+    </Typography>
+    <Button variant='contained'>New Firm</Button>
+    <Grid container spacing={2} mt={4}>
 
-    </div>
+{firms.map((firm,index)=>(  
+  <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+
+  <FirmsCard key={firm.id} firm={firm}/>
+
+  </Grid>
+  
+  ))}
+
+  
+
+</Grid>
+
+   </Container>
   )
 }
 
