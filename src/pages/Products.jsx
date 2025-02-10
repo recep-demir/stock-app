4b171d4039e54dd2fb8fffb572535b2a8f2a769c
recep-demir <demir.rp@gmail.com>
2025-02-10 19:22:47 +0100

    add Products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div> <Container>
+    <Typography variant="h4" color="secondary.second" align="center">
+      Products
+    </Typography>
+    <Button variant="contained"  onClick={handleOpen}  >NEW FIRM</Button>
+    <Grid container spacing={2} mt={2}>
+      {firms.map((firm,index) => (
+        <Grid item xs={12} md={6} lg={4} xl={3} key={index} >
+        <FirmsCard  {...firm}  setInitialState={setInitialState} handleOpen={handleOpen}/>
+        </Grid>
+      ))}
+    </Grid>
+
+    <FirmModal  open={open}   handleClose={handleClose} initialState={initialState}   />
+
+  </Container>
+  </div>
   )
 }
 
