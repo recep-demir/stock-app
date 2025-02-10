ac6c1c9fe0e59b229ea5e74a65ac2d74032f67d3
recep-demir <demir.rp@gmail.com>
2025-02-10 20:46:50 +0100

    add getrowld

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,24 +1,30 @@
+import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import { Container } from '@mui/material'
 import React from 'react'
+import ProductsTable from '../components/Table/ProductsTable'
+import useStockCall from '../hook/useStockCall'
+import { useEffect } from 'react'
 
 const Products = () => {
+  const {getStockData}=useStockCall()
+
+  useEffect(()=>{
+    getStockData("products")
+    getStockData("brands")
+    getStockData("categories")
+  },[])
+
   return (
-    <div> <Container>
-    <Typography variant="h4" color="secondary.second" align="center">
-      Products
-    </Typography>
-    <Button variant="contained"  onClick={handleOpen}  >NEW FIRM</Button>
-    <Grid container spacing={2} mt={2}>
-      {firms.map((firm,index) => (
-        <Grid item xs={12} md={6} lg={4} xl={3} key={index} >
-        <FirmsCard  {...firm}  setInitialState={setInitialState} handleOpen={handleOpen}/>
-        </Grid>
-      ))}
-    </Grid>
-
-    <FirmModal  open={open}   handleClose={handleClose} initialState={initialState}   />
-
-  </Container>
-  </div>
+    <div>
+       <Container>
+      <Typography variant="h4" color="secondary.second" align="center">
+        Products
+      </Typography>
+      <Button variant="contained">NEW PRODUCT</Button>
+      <ProductsTable/>
+    </Container>/
+    </div>
   )
 }
 
